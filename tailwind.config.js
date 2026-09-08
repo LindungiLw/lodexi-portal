691055632834f3e9f887191f9eddd0755c01765f
RahmaLindungiLaowo <rahmalindungilaowo@gmail.com>
2026-09-09 00:07:31 +0700

    Feat: Setup React, Vite, and Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.jsx',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                lodex: {
+                    coral: '#F29191',
+                    'coral-light': '#F7ADAD',
+                    cyan: '#B1E5E6',
+                    'cyan-light': '#CCFBFA',
+                }
+            }
+        },
+    },
+
+    plugins: [forms],
+};
